2f75fab033a8e87fe396c4f58dbdfeecbaf1a11b
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-03-30 02:21:24 +0100

    test

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next';
-
+console.log("BUILD TIME CHECK: NEXT_PUBLIC_API_URL is", process.env.NEXT_PUBLIC_API_URL);
 const nextConfig: NextConfig = {
   /* config options here */
 };
